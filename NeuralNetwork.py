cd00cd347db3ce5ba0359d9056997a495112184f
And1210 <amf7crazy@gmail.com>
2018-12-27 20:13:23 -0500

    Refactored data inputs and documentation added
    Any 1D input (either numpy array or python list) can be passed into the network. It no longer has to be formatted in a specific way. Many comments added

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,28 +1,48 @@
+"""
+Created: July 2018
+Updated: December 2018
+
+@author: Andrew Farley
+
+
+"""
+
 import numpy as np
-            
-def createCol(data):
+
+#Checks if incoming data needs to be turned into column vectors, returns the correctly shaped data
+#data ----- a 1D numpy array or python list
+def checkReform(data):
     out = np.array(data)
-    out.reshape([len(data), 1])
-    return out
-    
+    if (out.ndim < 1):
+        return np.array([[]])
+    elif (out.ndim == 1):
+        shape = out.shape
+        out = out.reshape(shape[0], 1)
+        return out
+    else:
+        return out
 
 #Each layer of the Neural Network has properties, those are stored here    
 class Layer:
+    #inputSize ----- the number of inputs (or neurons) from the previous layer
+    #selfSize ----- the number of neurons on this layer
     def __init__(self, inputSize, selfSize):
         self.weights = 2 * np.random.rand(selfSize, inputSize) - 1  #giving random weights to start
         self.bias = 2 * np.random.rand(selfSize, 1) - 1 #giving random bias to start
         
+    #inputData ----- a 1D array or list of input values with same length as inputSize
     def compute(self, inputData):
-        out = self.weights.dot(inputData)  #feeding the given inputs through the current layer
-#        out = (out - np.amin(out)) #renormalizing the data so it's evenly distributed
-#        out = out / np.amax(out)
+        out = self.weights.dot(inputData)  #feeding the given inputs through the current layerout)
         out = out + self.bias
         out = 1 / (1 + np.exp(-out))    #sigmoid activation function
-#        out = np.exp(out) / np.sum(np.exp(out))
         return out
 
 #The Nerual Network architechture, contains functions to train the network and give a guess
 class NeuralNetwork:
+    #Constructor of the Neural Network
+    #inputNum ----- the number of inputs to the NN
+    #hiddenNum ----- a 1D list containing the number of neurons on each hidden layer, can be any size n where n > 1
+    #outputNum ----- the number of outputs from the NN
     def __init__(self, inputNum, hiddenNum, outputNum):
         #Setting up layers for network
         hiddenLayers = len(hiddenNum)
@@ -40,21 +60,24 @@ class NeuralNetwork:
         return self.learningRate
         
     #make a guess of what an output should be base on inputs
+    #inputData ----- a 1D array or list of input values with same length as inputNum
     def feedForward(self, inputData):
-        curData = inputData
+        curData = checkReform(inputData)
         index = 0
         for layer in self.layers:
             curData = layer.compute(curData)
             self.layerOut[index] = curData
             index = index + 1
-        guess = curData
-        return guess
+        probabilities = curData
+        return probabilities
     
     #train the neurons by changing weights and bias based on error from real output
+    #inputData ----- a 1D array or list of input values with same length as inputNum
+    #expectedOutput ----- a 1D array or list of expected output values for the input with same length as outputNum
     def train(self, inputData, expectedOutput):
         currentOutput = self.feedForward(inputData)
-        layerOutputs = [inputData] + self.layerOut
-        errorOutput = np.array(expectedOutput) - np.array(currentOutput)
+        layerOutputs = [checkReform(inputData)] + self.layerOut
+        errorOutput = checkReform(expectedOutput) - checkReform(currentOutput)
         
         for i in range(len(self.layerOut)):
             index = len(layerOutputs) - i - 1
@@ -64,38 +87,35 @@ class NeuralNetwork:
             self.layers[index-1].bias = self.layers[index-1].bias + gradient
             errorOutput = self.layers[index-1].weights.transpose().dot(errorOutput)
     
-    #get the weight change to maximize what the network is trained for
-    def getAdjustOutput(self, inputData):
-        expectedOutput = np.ones([self.outputNum, 1])
-        currentOutput = self.feedForward(inputData)
-        layerOutputs = [inputData] + self.layerOut
-        errorOutput = np.array(expectedOutput) - np.array(currentOutput)
-        
-        for i in range(len(self.layerOut)):
-            index = len(layerOutputs) - i - 1
-            errorOutput = self.layers[index-1].weights.transpose().dot(errorOutput)
-        adjustment = self.learningRate * errorOutput * (inputData * (1 - inputData))
-        return adjustment
+    #returns the index of the highest value in the output vector
+    #inputData ----- a 1D array or list of input values with same length as the inputNum
+    def guess(self, inputData):
+        prob = self.feedForward(inputData)
+        prob = np.array(prob)
+        return prob.argmax()
     
-#nn = NeuralNetwork(2, [5], 1)
-#
-##   np.array(inputData, dtype='f').transpose()
-#
-#data = []
-#data.append(np.array([[0, 1]], dtype='f').transpose())
-#data.append(np.array([[1, 0]], dtype='f').transpose())
-#data.append(np.array([[1, 1]], dtype='f').transpose())
-#data.append(np.array([[0, 0]], dtype='f').transpose())
-#out = [[[1]], [[1]], [[0]], [[0]]]
-#guess = nn.feedForward(data[0])
-#print(guess)
-#
-#import random as rand
-#for i in range(50000):
-#    index = rand.randint(0, 3)
-#    nn.train(data[index], out[index])
-#    
-#    
-#print('')
-#for ar in data:
-#    print(nn.feedForward(ar))
+    
+#------------------- TESTING --------------------
+#This code tests the Neural Network by training it to compute the XOR binary function (something not possible with a perceptron)
+
+#Initialize the network
+nn = NeuralNetwork(2, [5], 2) 
+
+#Setting up the training data
+data = []
+data.append(np.array([0, 1]))
+data.append(np.array([1, 0]))
+data.append(np.array([1, 1]))
+data.append(np.array([0, 0]))
+out = [[0, 1], [0, 1], [1, 0], [1, 0]] #The expected output for every input added above
+
+#Train the network
+import random as rand
+for i in range(50000):
+    index = rand.randint(0, 3)
+    nn.train(data[index], out[index])
+    
+#Print the guess for every input
+print('')
+for ar in data:
+    print(nn.guess(ar))
